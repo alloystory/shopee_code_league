89af55e6355edff646bbd1111d8dc692c319d2d2
Aloysius Chan <alloystory@gmail.com>
2020-08-03 00:33:52 +0800

    add algorithms

--- 3_algorithms/problem_1/script.py
+++ 3_algorithms/problem_1/script.py
@@ -1,7 +1,33 @@
 import time
+from pprint import pprint
 
 def solver():
-    pass
+    T = int(input())
+    dictionary = dict()
+    
+    for i in range(T):
+        print("Case {}:".format(i + 1))
+        N, Q = [int(i) for i in input().split()]
+
+        for _ in range(N):
+            split_item = input().split()
+            unique_subsequence = set()
+            for i in range(len(split_item)):
+                for j in range(i + 1, len(split_item) + 1):
+                    subsequence = " ".join(split_item[i:j])
+                    unique_subsequence.add(subsequence)
+            
+            for subsequence in unique_subsequence:
+                if subsequence not in dictionary:
+                    dictionary[subsequence] = 0
+                dictionary[subsequence] += 1
+        pprint(dictionary)
+        # for _ in range(Q):
+        #     query = input()
+        #     if query in dictionary:
+        #         print(dictionary[query])
+        #     else:
+        #         print(0)
 
 if __name__ == "__main__":
     start_time = time.time()
--- 3_algorithms/problem_2/script.py
+++ 3_algorithms/problem_2/script.py
@@ -1,8 +1,18 @@
 import time
 
 def solver():
-    pass
-
+    T = int(input())
+    for i in range(T):
+        S, N = [int(i) for i in input().split()]
+        prices = [int(i) for i in input().split()]
+        sorted_prices = sorted(prices)
+        
+        cost = 0
+        for j in range(round(N/2)):
+            cost += sorted_prices[j]
+        
+        print("Case {}: {}".format(i + 1, cost))
+        
 if __name__ == "__main__":
     start_time = time.time()
     solver()
